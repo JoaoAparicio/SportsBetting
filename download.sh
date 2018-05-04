d7a7822104663fac59395d9ed5b32fc2c4079a8a
Joao Aparicio <jpmn.aparicio@gmail.com>
2018-05-04 12:18:56 +0100

    download.sh checks if data/ folder exists and if it doesn't, create it

--- download.sh
+++ download.sh
@@ -1,3 +1,7 @@
+if [ ! -d "data/" ]; then
+  mkdir data
+fi
+
 wget "http://www.football-data.co.uk/mmz4281/1617/data.zip" -O "data/data1617.zip"
 wget "http://www.football-data.co.uk/mmz4281/1516/data.zip" -O "data/data1516.zip"
 wget "http://www.football-data.co.uk/mmz4281/1415/data.zip" -O "data/data1415.zip"
