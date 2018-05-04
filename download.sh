cb18b3b2784cf4c7ade4e2bddcea45fb80d0632e
Joao Aparicio <jpmn.aparicio@gmail.com>
2018-05-04 11:42:56 +0100

    first

---
+++ download.sh
@@ -0,0 +1,4 @@
+wget "http://www.football-data.co.uk/mmz4281/1617/data.zip" -O "data/data1617.zip"
+wget "http://www.football-data.co.uk/mmz4281/1516/data.zip" -O "data/data1516.zip"
+wget "http://www.football-data.co.uk/mmz4281/1415/data.zip" -O "data/data1415.zip"
+
